63957523a5523765bf3583242ebac2f51bc5743a
Sean Weeks <scw5452@psu.edu>
2022-10-14 23:53:44 -0400

    Created the SerachEngine and SearchEngineController Classes. Created methods for both classes and added necessary methods and attributes to the Lighth ouse Class.

---
+++ InformationRadarCore/InformationRadarCore/Controllers/SearchEngineController.cs
@@ -0,0 +1,18 @@
+﻿namespace InformationRadarCore.Controllers
+
+public class SearchEngineController
+{
+    SearchEngine searchEngine;
+    Lighthouse lighthouse;
+
+    public SearchEngineController ( Lighthouse lighthouse )
+    {
+        this.searchEngine = new SearchEngine ();
+        this.lighthouse = lighthouse;
+    }
+
+    public void RunSearch ()
+    {
+        this.searchEngine.RunSearch ( this.lighthouse );
+    }
+}
--- InformationRadarCore/InformationRadarCore/Models/Lighthouse.cs
+++ InformationRadarCore/InformationRadarCore/Models/Lighthouse.cs
@@ -14,6 +14,25 @@ namespace InformationRadarCore.Models
     [Index(nameof(InternalName), IsUnique = true)]
     public class Lighthouse
     {
+        private String lighthouseScript;
+        private string internalName;
+        private String writeError;
+        private String errorFile;
+        private bool enabled;
+        private LighthouseSize baseSize;
+
+        /// <summary>
+        /// Default constructor that requires the Name be supplied and unique, 
+        /// sets the estimated base size of the Lighthouse,
+        /// and whether or not the Lighthouse is enabled or not.
+        /// </summary>
+        public Lighthouse (String name, bool enabled, LighthouseSize size)
+        {
+            this.InternalName = name;
+            this.BaseSize = size;
+            this.Enabled = enabled;
+        }
+
         public int Id { get; set; }
 
         /// <summary>
@@ -23,7 +42,11 @@ namespace InformationRadarCore.Models
         /// </summary>
         [Required, MinLength(1), MaxLength(100)]
         [RegularExpression(@"^[a-zA-Z][a-zA-Z]*$")]
-        public string InternalName { get; set; }
+        public string InternalName 
+        {
+            get { return internalName; }
+            set { this.internalName = value; } 
+        }
 
         /// <summary>
         /// The human-readable title of the lighthouse
@@ -37,7 +60,11 @@ namespace InformationRadarCore.Models
         /// The estimated size of the lighthouse
         /// </summary>
         [Required]
-        public LighthouseSize BaseSize { get; set; }
+        public LighthouseSize BaseSize 
+        {
+            get { return this.baseSize; }
+            set { this.baseSize = value; } 
+        }
 
         /// <summary>
         /// How often the lighthouse gathers data (measured in seconds)
@@ -68,18 +95,65 @@ namespace InformationRadarCore.Models
         public DateTime? LastSentMessage { get; set; }
 
         /// <summary>
-        /// Whether or not the lighthouse should run
+        /// Sets the boolean value of whether or not the lighthouse should run
         /// </summary>
         [Required]
-        public bool Enabled { get; set; }
+        public bool Enabled 
+        {
+            get { return this.enabled; }
+            set { this.enabled = value; } 
+        }
 
         /// <summary>
         /// App users who will recieve a message
+        /// The set methods are supplied by the ICollection<t> methods and the get methods are supplied by the IEnumerator<T> methods
         /// </summary>
-        public ICollection<ApplicationUser> Recipients { get; set; }
+        public ICollection<ApplicationUser> Recipients { set; }
 
-        public ICollection<GoogleQuery> GoogleQueries { get; set; }
+        public IEnumerator<ApplicationUser> Recipients { get; }
 
-        public ICollection<Site> Sites { get; set; }
+        public ICollection<GoogleQuery> GoogleQueries { set; }
+
+        public IEnumerator<GoogleQuery> GoogleQueries { get; }
+
+        public ICollection<Site> Sites { set; }
+
+        public IEnumerator<Site> Sites { get; }
+
+        /// <summary>
+        /// Gets and Sets the location properties of the Lighthouse python script attribute.
+        /// e.g. "C:\Temp\[INTERNAL NAME]_lighthouse.py"
+        /// </summary>
+        public String LighthouseScript 
+        {
+            get {return this.lighthouseScript; }
+            set {this.LighthouseScript = value; } 
+        }
+
+        /// <summary>
+        /// Sets the writeError attribute and writes it to the error log file
+        /// </summary>
+        public void WriteError
+        {
+            set 
+            {
+                this.writeError = value;
+                this.WriteErrorOut ();
+            }
+        }
+
+        /// <summary>
+        /// Writes writeError attribute to the error log file
+        /// </summary>
+        private async Task WriteErrorOut()
+        {
+            //Open Error log and write file here
+            using StreamWriter file = new( DateTime, this.errorFile, append: true);
+            await file.WriteLineAsync ( this.writeError );
+            if (file.Dispose)
+            {
+                this.writeError = null;
+            }
+        }
     }
 }
